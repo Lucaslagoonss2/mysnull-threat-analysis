a177410f5478cd0ecceac08d0da3f62b3b9fde1b
lucas_lagoon <lucaslagoon15@gmail.com>
2026-05-18 19:40:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+from extractors.ioc_extractor import extract_iocs
 
+
+sample_text = """
+Connection to malicious domain:
+
+http://evil-malware.com
+
+Suspicious IP:
+185.243.115.84
+
+Another domain:
+hacker-control.net
+"""
+
+
+results = extract_iocs(sample_text)
+
+print("\nIOC RESULTS\n")
+
+print("IPs:")
+print(results["ips"])
+
+print("\nDomains:")
+print(results["domains"])
+
+print("\nURLs:")
+print(results["urls"])
